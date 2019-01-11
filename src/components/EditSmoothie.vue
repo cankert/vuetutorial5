6178ee9c7ab7aff439e3ecefbdad92ea2c5f7ab8
cankert <ankert.c@gmail.com>
2019-01-11 15:34:04 +0100

    ghgh

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="edit-smoothie container">
+    <h2 class="center-align indigo-text">EditSmoothie Recipe</h2>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "EditSmoothie",
+  data() {
+    return {
+     
+    };
+  },
+  methods: {
+    
+}
+}
+<style>
+</style>
